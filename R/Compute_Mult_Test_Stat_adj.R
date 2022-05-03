84c74f4e246eee4e19bea3dbc8ab1c5ee8fe3d84
yukamoro <ymoroish@gmail.com>
2022-05-03 14:31:03 -0400

    debug

--- R/Compute_Mult_Test_Stat_adj.R
+++ R/Compute_Mult_Test_Stat_adj.R
@@ -11,22 +11,67 @@
 #' @export
 
 Compute_Mult_Test_Stat_adj = function(exposure, outcome, confounder,mediator){
-  #Fit exposure on mediators
-  fit1 = glm(exposure ~ mediator + confounder,
-             family = "binomial")
-  #Compute inverse odds weights
-  predprob = predict(fit1, type = "response")
-  predprob = ifelse(predprob>1, 1, predprob)
-  invodds = ((1-predprob)/predprob)
-  invodd_weights = ifelse(exposure == 0, 1, invodds)
-  #Fit outcome on exposure with weights for direct effect
-  fit2 = glm(outcome ~ exposure + confounder,
-             family = "quasipoisson",
-             weights = invodd_weights)
-  #Fit outcome on exposure for total effect
-  fitte = glm(outcome ~ exposure + confounder,
-              family = "quasipoisson")
-  #Calculate indirect effect
+
+  if(is.null(confounder)){
+    #mediator = meddata[,1]
+    fit1 = glm(exposure ~ mediator,
+               family = "binomial")
+    #Compute an IORW/IOW weight
+    predprob = predict(fit1, type = "response")
+    predprob = ifelse(predprob>1, 1, predprob)
+    invodds = ((1-predprob)/predprob)
+    invodd_weights = ifelse(exposure == 0, 1, invodds)
+
+    #Check if dichotomous or continuous
+    if(length(table(outcome))==2){
+      #Fit outcome on exposure with weights for direct effect
+      fit2 = glm(outcome ~ exposure ,
+                 family = "quasipoisson",
+                 weights = invodd_weights)
+      #Fit outcome on exposure for total effect
+      fitte = glm(outcome ~ exposure ,
+                  family = "quasipoisson")
+    } else{
+      #Fit outcome on exposure with weights for direct effect
+      fit2 = glm(outcome ~ exposure ,
+                 weights = invodd_weights)
+      #Fit outcome on exposure for total effect
+      fitte = glm(outcome ~ exposure)
+    }
+
+  }
+  else{
+    #Fit exposure on outcome
+    #mediator = meddata[,1]
+    fit1 = glm(exposure ~ mediator + confounder,
+               family = "binomial")
+    #Compute an IORW/IOW weight
+    predprob = predict(fit1, type = "response")
+    predprob = ifelse(predprob>1, 1, predprob)
+    invodds = ((1-predprob)/predprob)
+    invodd_weights = ifelse(exposure == 0, 1, invodds)
+
+    #Check if dichotomous or continuous
+    if(length(table(outcome))==2){
+      #Fit outcome on exposure with weights for direct effect
+      fit2 = glm(outcome ~ exposure + confounder,
+                 family = "quasipoisson",
+                 weights = invodd_weights)
+      #Fit outcome on exposure for total effect
+      fitte = glm(outcome ~ exposure + confounder,
+                  family = "quasipoisson")
+    } else{
+      #Fit outcome on exposure with weights for direct effect
+      fit2 = glm(outcome ~ exposure + confounder,
+                 weights = invodd_weights)
+      #Fit outcome on exposure for total effect
+      fitte = glm(outcome ~ exposure + confounder)
+    }
+
+  }
+
+
+  #Calculate indirect effect (mediation)
   ie = fitte$coefficients[2] - fit2$coefficients[2]
   return(ie)
 }
--- R/HTMMIOW.R
+++ R/HTMMIOW.R
@@ -13,9 +13,8 @@
 #' @param B number of replicates for hypothesis test
 #' @return A list of observed test statistic, p-value, and test statistics under the null hypothesis
 #' @export
-#' @importFrom compositions clr
+#' @importFrom compositions ilr
 #' @importFrom umap umap
-#' @importFrom stats prcomp
 #' @examples
 #' Perform_HTMMIOW(exposure = sim_data$sim_exposure,
 #' outcome = sim_data$sim_outcome,
@@ -33,42 +32,29 @@ Perform_HTMMIOW = function(exposure,
   #Impute 0 values with pseudocount
   mediator_imp = mediator
   mediator_imp[mediator_imp==0] = 0.5
-  #clr transform
-  clr_trans = clr(mediator_imp)
-  #SVD
-  pca_mat  = prcomp(clr_trans)
+  #ilr transform
+  ilr_trans = ilr(mediator_imp)
   #UMAP
-  umap_mat = umap(pca_mat$x, n_components = umapcomponents)
+  umap_mat = umap(matrix(ilr_trans, nrow=nrow(ilr_trans)), n_components = umapcomponents)
   transmedmat = umap_mat$layout
 
-  #Perform hypothesis test
-  if(is.null(confounder)){
-    #Observed Test Statistic (Indirect Effect)
-    tk = Compute_Mult_Test_Stat(exposure, outcome, transmedmat)
-    #Compute statistics under the null hypothesis
-    perm_null = replicate(B,
-                          Compute_Mult_Test_Stat(exposure,
-                                                 outcome,
-                                                 transmedmat[(sample(c(1:nrow(transmedmat)), nrow(transmedmat), replace = FALSE)),]))
-    #Calculate P-value
-    pk_2 = (sum(abs(perm_null) > abs(tk))/B)
-  }
-  else{
-    #Observed Test Statistic (Indirect Effect)
-    tk = Compute_Mult_Test_Stat_adj(exposure, outcome, confounder, transmedmat)
-    #Compute statistics under the null hypothesis
-    perm_null = replicate(B,
-                          Compute_Mult_Test_Stat_adj(exposure,
-                                                     outcome,
-                                                     confounder,
-                                                     transmedmat[(sample(c(1:nrow(transmedmat)), nrow(transmedmat), replace = FALSE)),]))
-    #Calculate P-value
-    pk_2 = (sum(abs(perm_null) > abs(tk))/B)
-  }
+  #Hypothesis test
+  #print("Performing Hypothesis Test...")
+  #Test Statistic
+  tk = Compute_Mult_Test_Stat_adj(exposure, outcome, confounder, transmedmat)
+  #Compute Nulls
+  perm_null = replicate(B, Compute_Mult_Test_Stat_adj(exposure,
+                                                      outcome,
+                                                      confounder,
+                                                      transmedmat[(sample(c(1:nrow(transmedmat)), nrow(transmedmat), replace = FALSE)),]))
+  #Calculate Pvalue
+  pk_2 = (sum(abs(perm_null) > abs(tk))/B)
+
   #Create Output
   output = list(tk = tk,
                 pk_2 = pk_2,
                 perm_null = perm_null)
+
   return(output)
 }
 
